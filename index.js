bdfbdd8b581e3b73ef91e29ca2cd0e91b6de8e66
codingspirits <vincci.cheng.9@gmail.com>
2021-07-18 23:23:32 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+/*const products = [
+    { name: 'Shampoo', price: 4.99 },
+    { name: 'Donuts', price: 7.99 },
+    { name: 'Cookies', price: 6.49 },
+    { name: 'Bath Gel', price: 13.99 }
+  ];
+  
+function currentSpending(products){
+    let totalPrice = 0
+    products.forEach(function(product){
+        totalPrice = product.price + totalPrice
+    })
+    console.log(totalPrice)
+    }
+currentSpending(products)*/
 
-// Code your solution here
+  const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+let totalBatteries = batteryBatches.reduce(function(total,element){
+    return element + total})
